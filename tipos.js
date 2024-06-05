2aacf0568f6588fc4b04a466b6fed34d5a2bcd19
Kiara <kiara.almeidaaf@gmail.com>
2024-06-05 12:32:22 -0300

    aula de programação

---
+++ tipos.js
@@ -0,0 +1,9 @@
+let frutas = "limão " 
+let legumes = ["alface", "tomate", "cenoura" ]
+let banana = {
+    "cor": "amarela",
+    "leguminosa": "não",
+    "fruta": "sim",
+    "sabor": "doce"
+}
+console.log(banana.cor)
