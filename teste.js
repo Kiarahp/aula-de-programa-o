5888c57b62279ca464408bfa2b36646f30ac80cf
Kiara <kiara.almeidaaf@gmail.com>
2024-07-10 12:29:53 -0300

    aula de interpretação

---
+++ teste.js
@@ -0,0 +1,3 @@
+//Eu musico uma corda do violão partiu buscar 3 loja eu estou canela lojas no avenida sete piedade gamboa mais barata longe quero barata onde ir
+let lojasviolao = ["avenida 7", "piedade", "gamboa"] 
+let baratoPertoDo = ["canela"]
