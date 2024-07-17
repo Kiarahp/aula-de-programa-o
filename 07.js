e65426e8787d8641f61a4ac2e44d33c41684b84c
Kiara <kiara.almeidaaf@gmail.com>
2024-07-17 12:16:02 -0300

    aula de interprentação 2

---
+++ 07.js
@@ -0,0 +1,10 @@
+//calca 4 bolso 2 1 fundo 1 bolso celular carteira chave 2 bolso outra chave cartao de visita e um cadeado
+let calça = {
+    "bolso1": ["celular", "carteira", "chave"],
+    "bolso2": ["chave", "cartao de visita", "cadeado"],
+    "bolso3": ["nada"],
+    "bolso4": ["nada"]
+}
+
+
+console.log(calça)
