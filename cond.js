add1f20168a8c92a48bf5b6b493c033b17f27f4e
Kiara <kiara.almeidaaf@gmail.com>
2024-07-31 12:37:16 -0300

    aula de condicional

---
+++ cond.js
@@ -0,0 +1,21 @@
+// se voce fizer o dever podera usar o celular
+/*let pessoa = {
+    "dever": "nao fez"
+}
+console.log(pessoa)
+if(
+    pessoa.dever == "nao fez"
+) {
+    console.log("nao pode usar o celular")
+}
+*/
+// se pessoa atravassar na faixa pode, nao atravessar nao pode
+let pessoa = {
+    "atravessar_na_faixa": "nao"
+}
+console.log(pessoa)
+if(
+    pessoa.atravessar_na_faixa == "sim"
+) {
+    console.log("pode")
+}
