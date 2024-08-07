a2e214dc605354454f116797dd8e8cf53aa497d6
Kiara <kiara.almeidaaf@gmail.com>
2024-08-07 12:35:33 -0300

    aula de for

---
+++ for.js
@@ -0,0 +1,5 @@
+let nomes = ["ana", "bia", "carlos", "davi", "eva", "flavia", "gabi", "heloisa", "icaro", "joao", "kiara"]
+for(var i = 0; i < 5; i++) {
+    console.log(i)
+    console.log(nomes[i])
+} 
